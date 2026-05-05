2545a4ce6b47828abd0201a94b7bb06e35b44dc9
crypdoughdoteth <zkorn1@icloud.com>
2026-05-05 15:10:15 -0400

    Update dependencies, breaking changes in rand, update authorship & licensing with attribution

--- src/nonce.rs
+++ src/nonce.rs
@@ -1,9 +1,8 @@
-use rand::distributions::Alphanumeric;
-use rand::{Rng, thread_rng};
+use rand::{RngExt, distr::Alphanumeric, rng};
 
 /// Generates a secure nonce.
 pub fn generate_nonce() -> String {
-    thread_rng()
+    rng()
         .sample_iter(&Alphanumeric)
         .take(17)
         .map(char::from)
